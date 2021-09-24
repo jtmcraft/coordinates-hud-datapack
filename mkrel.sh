781fdfe68922ba2b04f72499faa83d8dda5a482f
jtmcraft <jt_mcraft@outlook.com>
2021-09-24 14:32:19 -0600

    reorganize

--- mkrel.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf ./release
-mkdir -p ./release/coordinates-hud-datapack-1.18
-cp pack.mcmeta ./release/coordinates-hud-datapack-1.18
-cp -r data ./release/coordinates-hud-datapack-1.18
